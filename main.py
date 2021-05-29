144b4d94bef9a753e6dc538a74e0f98448295302
unknown <martawei19@gmail.com>
2021-05-29 19:10:13 +0200

    Pierwsza zmiana

--- main.py
+++ main.py
@@ -17,3 +17,44 @@ if tab_suma[2] == 0:
 else:
     print("NIEPOPRAWNY")
 
+x = 10
+if tab_pesel[9] % 2==0:
+    print("Osoba z PESELem ", pesel, " jest kobieta")
+else:
+    print("Osoba z PESELem ", pesel, "jest mezczyzna")
+
+rok = tab_pesel[0:2]
+miesiac = tab_pesel[2:4]
+dzien = tab_pesel[4:6]
+
+r = (''.join([str(i) for i in rok]))
+msc = (''.join([str(i) for i in miesiac]))
+d = (''.join([str(i) for i in dzien]))
+
+if msc == "01":
+    m = "styczeń"
+elif msc == "02":
+    m = "luty"
+elif msc == "03":
+    m = "marzec"
+elif msc == "04":
+    m = "kwiecień"
+elif msc == "05":
+    m = "maj"
+elif msc == "06":
+    m = "czerwiec"
+elif msc == "07":
+    m = "lipiec"
+elif msc == "08":
+    m = "sierpien"
+elif msc == "09":
+    m = "wrzesien"
+elif msc == "10":
+    m = "pazdziernik"
+elif msc == "11":
+    m = "listopad"
+elif msc == "12":
+    m = "grudzien"
+
+
+print("Ta osoba urodziła się", d, "dnia, ", "miesiaca ", m, " ,", r, "roku")
